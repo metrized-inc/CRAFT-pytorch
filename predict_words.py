0b5a8bb9f7758c2941e378cd95fb942bb34b9eb5
Ho Jin Chang <72773536+hojinchang@users.noreply.github.com>
2022-11-02 10:31:15 -0700

    Created script for only 4 stage STR predictions

---
+++ predict_words.py
@@ -0,0 +1,52 @@
+import argparse
+import string
+import torch
+import torch.backends.cudnn as cudnn
+
+from deep_text_recognition_utils.deep_text_recognition import run_str
+
+
+def main(args):
+    run_str(args, args.image_folder)
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='CRAFT Text Detection')
+
+    # 4 Stage Deep Scene Recognition
+    parser.add_argument('--image_folder', required=True, help='path to image_folder which contains text images')
+    parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument('--batch_size', type=int, default=192, help='input batch size')
+    parser.add_argument('--str_model', required=True, help="path to STR saved model to evaluation")
+    """ Data processing """
+    parser.add_argument('--batch_max_length', type=int, default=25, help='maximum-label-length')
+    parser.add_argument('--imgH', type=int, default=32, help='the height of the input image')
+    parser.add_argument('--imgW', type=int, default=100, help='the width of the input image')
+    parser.add_argument('--rgb', action='store_true', help='use rgb input')
+    parser.add_argument('--character', type=str, default='0123456789abcdefghijklmnopqrstuvwxyz', help='character label')
+    parser.add_argument('--sensitive', action='store_true', help='for sensitive character mode')
+    parser.add_argument('--PAD', action='store_true', help='whether to keep ratio then pad for image resize')
+    """ Model Architecture """
+    parser.add_argument('--Transformation', type=str, required=True, help='Transformation stage. None|TPS')
+    parser.add_argument('--FeatureExtraction', type=str, required=True, help='FeatureExtraction stage. VGG|RCNN|ResNet')
+    parser.add_argument('--SequenceModeling', type=str, required=True, help='SequenceModeling stage. None|BiLSTM')
+    parser.add_argument('--Prediction', type=str, required=True, help='Prediction stage. CTC|Attn')
+    parser.add_argument('--num_fiducial', type=int, default=20, help='number of fiducial points of TPS-STN')
+    parser.add_argument('--input_channel', type=int, default=1, help='the number of input channel of Feature extractor')
+    parser.add_argument('--output_channel', type=int, default=512,
+                        help='the number of output channel of Feature extractor')
+    parser.add_argument('--hidden_size', type=int, default=256, help='the size of the LSTM hidden state')
+
+    args = parser.parse_args()
+
+    """ STR vocab / character number configuration """
+    if args.sensitive:
+        args.character = string.printable[:-6]  # same with ASTER setting (use 94 char).
+
+    cudnn.benchmark = True
+    cudnn.deterministic = True
+    args.num_gpu = torch.cuda.device_count()
+
+    main(args)
